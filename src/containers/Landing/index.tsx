3c8f66872059bfc165c4df708aa8cb2779af0e65
gabrielsanttana <gabriel.gsantana7@gmail.com>
2020-10-13 22:40:19 -0300

    Create lading page structure and styling

--- src/containers/Landing/index.tsx
+++ src/containers/Landing/index.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import {FaArrowRight} from 'react-icons/fa';
+import logo from '../../assets/logo.svg';
 import styles from './Landing.module.scss';
 
 const Landing: React.FC = () => {
   return (
     <div className={styles.landing}>
-      <h1>Landing</h1>
+      <div className={styles.landingWrapper}>
+        <header>
+          <img src={logo} alt="Happy Logo" />
+
+          <div>
+            <strong>Americana</strong>
+            <span>São Paulo</span>
+          </div>
+        </header>
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+        </main>
+
+        <footer>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+
+          <Link to="/">
+            <FaArrowRight color="#8D734B" size={24} />
+          </Link>
+        </footer>
+      </div>
     </div>
   );
 };
