8e82f29a652203173fa306868deeeb25835f22bd
gabrielsanttana <gabriel.gsantana7@gmail.com>
2020-11-28 15:36:28 -0300

    Backend initial commit

---
+++ backend/src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+  response.json({
+    message: 'Hello, World!',
+  });
+});
+
+app.listen('3333', () => {
+  console.log('⚙️  Server is running on port 3333');
+});
