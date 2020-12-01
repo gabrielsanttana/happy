bf7a744de0a554b77e53792a8c13ca4b032f7b1a
gabrielsanttana <gabriel.gsantana7@gmail.com>
2020-12-01 16:55:15 -0300

    Add CORS config

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import path from 'path';
 import 'express-async-errors';
 import './database/connection';
@@ -7,6 +8,7 @@ import router from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
