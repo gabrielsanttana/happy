bdb1bfc848b4d0760e7db098602f1c5ed0d87574
gabrielsanttana <gabriel.gsantana7@gmail.com>
2020-12-01 15:52:43 -0300

    Add async error handler

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import path from 'path';
+import 'express-async-errors';
 import './database/connection';
+import errorHandler from './errors/handler';
 import router from './routes';
 
 const app = express();
@@ -8,6 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(router);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
 
 app.listen('3333', () => {
   console.log('⚙️  Server is running on port 3333');
